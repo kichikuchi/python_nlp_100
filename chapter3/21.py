f62a72d7110e4bb2adf55d2571dffd74afc60814
kichikuchi <kikuchi0111@gmail.com>
2018-02-11 14:57:22 +0900

    add 21

---
+++ chapter3/21.py
@@ -0,0 +1,15 @@
+import json
+
+f = open('jawiki-country.json')
+flist = list(f)
+
+england = ""
+for item in flist:
+    jsonData = json.loads(item)
+    if jsonData["title"] == "イギリス":
+        england = jsonData["text"]
+
+lines = england.split('\n')
+for line in lines:
+    if line.count('Category'):
+        print(line)
