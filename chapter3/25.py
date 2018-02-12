c930ea51568a551059ae66fb3b399628c852f067
kichikuchi <kikuchi0111@gmail.com>
2018-02-12 10:29:58 +0900

    add 25

---
+++ chapter3/25.py
@@ -0,0 +1,32 @@
+import json
+import re
+
+f = open('jawiki-country.json')
+flist = list(f)
+
+england = ""
+for item in flist:
+    jsonData = json.loads(item)
+    if jsonData["title"] == "イギリス":
+        england = jsonData["text"]
+
+lines = england.split('\n')
+flag = False
+
+dic = {}
+for line in lines:
+    if flag:
+        if re.search('\|(.+)\=', line):
+            m = re.search('\|(.+)\=', line)
+            key = line[m.start():m.end()]
+            key = key[1:-2]
+            value = line[m.end():]
+            dic[key] = value
+
+    if re.search('\{\{基礎情報', line):
+        flag = True
+    
+    if re.search('^\}\}$', line):
+        flag = False
+
+print(dic)
