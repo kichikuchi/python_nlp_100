da63246a10023a8199b3e531cfc8f2090db3654c
kichikuchi <kikuchi0111@gmail.com>
2018-02-12 11:54:23 +0900

    add 27

---
+++ chapter3/27.py
@@ -0,0 +1,42 @@
+import json
+import re
+
+f = open('jawiki-country.json')
+flist = list(f)
+
+england = ""
+for item in flist:
+    jsonData = json.loads(item)
+    if jsonData["title"] == "イギリス":
+        england = jsonData["text"]
+
+lines = england.split('\n')
+flag = False
+
+dic = {}
+for line in lines:
+    if flag:
+        if re.search('\|(.+)\=', line):
+            m = re.search('\|(.+)\=', line)
+            key = line[m.start():m.end()]
+            key = key[1:-2]
+            value = line[m.end():]
+            value = value.replace('\'\'\'', '')
+            if re.search('\[\[(.+)\]\]', value):
+                print(value)
+                value = value.replace('[', '')
+                value = value.replace(']', '')
+                print(value)
+                # m = re.search('\[\[(.+)\]\]', value)
+                # print(m.group())
+
+            dic[key] = value
+
+    if re.search('\{\{基礎情報', line):
+        flag = True
+    
+    if re.search('^\}\}$', line):
+        flag = False
+
+# for k, v in dic.items():
+    # print(k, v)
