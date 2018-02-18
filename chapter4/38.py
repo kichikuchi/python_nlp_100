b24a8a923a526a33827e264b0a5422d24ff5b8a3
kichikuchi <kikuchi0111@gmail.com>
2018-02-18 12:32:27 +0900

    add 38

---
+++ chapter4/38.py
@@ -0,0 +1,43 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.font_manager import FontProperties
+
+fp = FontProperties(fname="ipag.ttf", size=14)
+
+f = open('neko.txt.mecab')
+
+book = []
+sentence = []
+
+for line in f:
+    if line.count("EOS"):
+        book.append(sentence)
+        sentence = []
+    else:
+        surface = line.split("\t")[0]
+        morphemes = line.split("\t")[1].split(",")
+        base = morphemes[5]
+        pos = morphemes[0]
+        pos1 = morphemes[1]
+        dic = {"surface": surface, "base": base, "pos": pos, "pos1": pos1}
+        sentence.append(dic)
+
+dicCount = {}
+for i in book:
+    for element in i:
+        key = element["surface"]
+        if key in dicCount.keys():
+            dicCount[key]+=1
+        else:
+            dicCount[key] = 1
+
+sorted = sorted(dicCount.items(), key=lambda x: -x[1])
+keys = []
+values = []
+
+for i in sorted:
+    keys.append(i[0])
+    values.append(i[1])
+
+plt.hist(values, range=(20, 100), bins=20)
+plt.show()
