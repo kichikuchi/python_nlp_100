9057900e8ee98c58447896887bdb02a7205ce0f6
kichikuchi <kikuchi0111@gmail.com>
2018-02-18 11:15:58 +0900

    add 35

---
+++ chapter4/35.py
@@ -0,0 +1,28 @@
+f = open('neko.txt.mecab')
+
+book = []
+sentence = []
+
+for line in f:
+    if line.count("EOS"):
+        book.append(sentence)
+        sentence = []
+    else:
+        surface = line.split("\t")[0]
+        morphemes = line.split("\t")[1].split(",")
+        base = morphemes[5]
+        pos = morphemes[0]
+        pos1 = morphemes[1]
+        dic = {"surface": surface, "base": base, "pos": pos, "pos1": pos1}
+        sentence.append(dic)
+
+nouns = []
+for i in book:
+    for (index, element) in enumerate(i):
+        if (element["pos"] == "名詞"): 
+            nouns.append(element["surface"])
+        else:
+            if len(nouns) > 1:
+                print("".join(nouns))
+                nouns = []
+
